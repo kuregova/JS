9e3694ca2862fdf365f1b529a02acdafc4508d21
n <n@gmail.com>
2022-09-16 15:59:10 +0300

    final

---
+++ script.js
@@ -0,0 +1,39 @@
+var name = prompt('Ведите свое имя');
+console.log(name);
+
+var age = prompt('Введите свой возраст')
+console.log(age)
+
+console.log(6 + 3)
+var plus = prompt('Решите данный пример: 6 + 3 =')
+console.log(plus)
+
+
+
+console.log(6 - 3)
+var minus = prompt('Решите данный пример: 6 - 3 =')
+console.log(minus)
+
+console.log(6 / 3)
+var divibe = prompt('Решите данный пример: 6 / 3 =')
+console.log(divibe)
+
+console.log(6 * 3)
+var multiply = prompt('Решите данный пример: 6 * 3 =')
+console.log(multiply)
+
+console.log(7 % 3)
+var residue = prompt('Решите данный пример: 7 / 3 =')
+console.log(residue)
+
+var answers = prompt('Зайдите в консоль, чтобы увидить правильные ответы')
+console.log(answers)
+
+
+var X = prompt('Введите первое число')
+var Y = prompt('Введите второе число')
+var Z = prompt ('Ведите третье число')
+
+var res = 200
+console.alert()
+
